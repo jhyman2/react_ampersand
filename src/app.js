539d01b02bf131a48fab2b7d0807f8fc8f8ae8f9
Jamison Hyman <jhyman2@umbc.edu>
2015-08-06 10:24:42 -0700

    Webpacked hello world

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var React = require('react')
+
+var Hello = React.createClass({
+  displayName: 'Hello',
+  render: function(){
+    return <div>Hello, {this.props.name}</div>
+  }
+})
+
+React.render(<Hello name='World!'/>, document.body)
