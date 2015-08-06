99a2b84326157e8c76598ba1bcf5fcf889985133
Jamison Hyman <jhyman2@umbc.edu>
2015-08-06 13:31:31 -0700

    Styling

--- src/pages/public.js
+++ src/pages/public.js
@@ -3,6 +3,19 @@ import React from 'react'
 export default React.createClass({
   displayName: 'PublicPage',
   render () {
-    return <h1>Public page</h1>
+    return (
+      <div className='container'>
+        <header role='banner'>
+          <h1>Labelr</h1>
+        </header>
+        <div>
+          <p>We label stuff for you and stuff&trade;</p>
+          <a href='/repos' className='button button-large'>
+            <span className='mega-octicon octicon-mark-github'></span>
+            Login with Github
+          </a>
+        </div>
+      </div>
+    )
   }
 })
