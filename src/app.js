4ac859a510035da9bbb88750b0d0c551b7769f79
Jamison Hyman <jhyman2@umbc.edu>
2015-08-06 13:40:23 -0700

    Using ampersand-app...seems cool now, this is the end of the free react_ampersand tutorial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
+import app from 'ampersand-app'
 import styles from './styles/main.styl'
 import Router from './router'
 
-window.app = {
+
+// as opposed to window.app = {}...
+// one advantage of ampersand
+// @todo how is this done in backbone?
+
+app.extend({
   init() {
     this.router = new Router()
     this.router.history.start()
   }
-}
+})
 
-window.app.init()
+app.init()
--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+import app from 'ampersand-app'
 import React from 'react'
 import Router from 'ampersand-router'
 import PublicPage from './pages/public'
